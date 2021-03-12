a8b5c02df09a1436a2e579eb5d292975287dc3b3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-03-12 17:47:09 +0100

    feat: first lesson

---
+++ first-lesson.py
@@ -0,0 +1,6 @@
+firstName = raw_input('What is your first name?: ')
+lastName = raw_input('What is your last name?: ')
+age = raw_input('Whats is your age?: ')
+
+
+print 'Hi, my name is ' + firstName + ' ' + lastName + ', and I am ' + str(age)
